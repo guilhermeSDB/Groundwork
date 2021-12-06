f014ac401c9a2a058624745a26d52852cdd59170
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2021-12-05 21:13:40 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/index.html',
+    './src/style.css',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
